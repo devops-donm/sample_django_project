e988c6881e037de0dbdad4cfa26855bf2dca8229
DevOps Don <devops-don@proton.me>
2024-11-06 13:57:07 -0500

    Update generate_ssl.sh

--- generate_ssl.sh
+++ generate_ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables (edit these as needed)
-DOMAIN="test.com"              # Primary domain
-ALT_DOMAIN="www.test.com"      # Alternative domain
+DOMAIN="test.local"              # Primary domain
+ALT_DOMAIN="www.test.local"      # Alternative domain
 CERT_DIR="/opt/docker_volume/nginx/certs"  # Directory to store the certificate
 KEY_FILE="${CERT_DIR}/${DOMAIN}.key"
 CERT_FILE="${CERT_DIR}/${DOMAIN}.crt"
