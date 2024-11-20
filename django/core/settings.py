d37c6cb00b9fee672896ef591a827dd7a4cf2f2a
DevOps Don <devops-don@proton.me>
2024-11-19 19:16:18 -0500

    fixed security based variables in settings

--- django/core/settings.py
+++ django/core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m##0lcfx#v-=9w=$jg4oz58o255jxbgo)bh9(63dj_t34f$grq'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
